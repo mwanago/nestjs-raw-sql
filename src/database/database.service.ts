46e77c60423e259a6ad353ddd832a2ffd6269610
marcin <wanago.marcin@gmail.com>
2022-08-25 19:22:15 +0200

    feat(Database): return just rows from the runQuery method

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -6,8 +6,9 @@ import { CONNECTION_POOL } from './database.module-definition';
 class DatabaseService {
   constructor(@Inject(CONNECTION_POOL) private readonly pool: Pool) {}
 
-  runQuery(query: string, params?: unknown[]) {
-    return this.pool.query(query, params);
+  async runQuery(query: string, params?: unknown[]) {
+    const queryResponse = await this.pool.query(query, params);
+    return queryResponse.rows;
   }
 }
 
