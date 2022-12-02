9ba267cad0273e35ac8a43f45cd38ca436194bbf
marcin <wanago.marcin@gmail.com>
2022-12-02 04:20:35 +0100

    feat(DatabaseModule): implement logging functionalities

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,17 +1,60 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { Pool } from 'pg';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Pool, PoolClient } from 'pg';
 import { CONNECTION_POOL } from './database.module-definition';
 
 @Injectable()
 class DatabaseService {
+  private readonly logger = new Logger('SQL');
   constructor(@Inject(CONNECTION_POOL) private readonly pool: Pool) {}
 
   async runQuery(query: string, params?: unknown[]) {
-    return this.pool.query(query, params);
+    return this.queryWithLogging(this.pool, query, params);
+  }
+
+  getLogMessage(query: string, params?: unknown[]) {
+    if (!params) {
+      return `Query: ${query}`;
+    }
+    return `Query: ${query} Params: ${JSON.stringify(params)}`;
+  }
+
+  async queryWithLogging(
+    source: Pool | PoolClient,
+    query: string,
+    params?: unknown[],
+  ) {
+    const queryPromise = source.query(query, params);
+
+    // message without unnecessary spaces and newlines
+    const message = this.getLogMessage(query, params)
+      .replace(/\n|/g, '')
+      .replace(/  +/g, ' ');
+
+    queryPromise
+      .then(() => {
+        this.logger.log(message);
+      })
+      .catch((error) => {
+        this.logger.warn(message);
+        throw error;
+      });
+
+    return queryPromise;
   }
 
   async getPoolClient() {
-    return this.pool.connect();
+    const poolClient = await this.pool.connect();
+
+    return new Proxy(poolClient, {
+      get: (target: PoolClient, propertyName: keyof PoolClient) => {
+        if (propertyName === 'query') {
+          return (query: string, params?: unknown[]) => {
+            return this.queryWithLogging(target, query, params);
+          };
+        }
+        return target[propertyName];
+      },
+    });
   }
 }
 
